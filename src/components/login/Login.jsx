2e46106206793af7a59872168cdb78c333ec2c5e
Sebastian barrera <sebasbh2008@hotmail.com>
2023-08-26 17:25:42 -0500

    funcion slider

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import logo from'../../assets/imagenes/logo.png'
+import telefono from'../../assets/imagenes/telefono.png'
+import linea from'../../assets/imagenes/linea-amarilla.png'
+import celular from'../../assets/imagenes/barraCelular.png'
+import './login.scss'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <section className='container__login'>
+      <img src={celular} alt="celuco" className='cel'/>
+      <img src={logo} alt="logo" />
+      <h3>Sing in</h3>
+      <p>Login or create an account with your phone number to start ordering</p>
+      <div className='div__inputs'>
+        <img src={telefono} alt="" />
+        <select name="" id="">
+          <option value="colombia">+ 57</option>  
+          <option value="venezuela">+ 58</option>
+          <option value="ecuador">+56</option>
+          <option value="usa">+ 1</option>
+          <option value="españa"> + 2</option>
+          <option value="alemania">+ 15</option>🇧🇴 🇦🇲
+        </select>
+        <input type="number" name="" id="" />
+      </div>
+      <img src={linea} alt="linea" />
+      <div className='advertencia'>
+        <span className='derechos__autor'>By clicking the button next you accept</span>
+        <span className='derechos__autor'>Terms of use</span>
+      </div>
+      <button className='btn__next1' >login</button>
+    </section>
   )
 }
 
--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,21 +1,34 @@
 import './slider.scss'
-import React from 'react'
+import React, { useRef } from 'react'
 import barraCelular from "../../assets/imagenes/barraCelular.png"
 import mujerCelular from "../../assets/imagenes/mujerCelular.png"
 import hombreCelular from "../../assets/imagenes/hombreCelular.png"
 import mujerCelular2 from "../../assets/imagenes/mujer2Celular.png"
 
 import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import { Pagination } from 'swiper/modules';
+import Login from '../login/Login'
 
 const Slider = () => {
+    const swiperRef = useRef(null);    
+
+    SwiperCore.use([Pagination]);
+
+    const siguiente = () => {
+        if (swiperRef.current && swiperRef.current.swiper) {
+            swiperRef.current.swiper.slideNext(); 
+        }
+    }
+
+
 
     return (
 
         <Swiper
-
+            ref={swiperRef} 
             pagination={{
                 dynamicBullets: true,
             }}
@@ -29,7 +42,7 @@ const Slider = () => {
                         <img src={mujerCelular} alt="Mujer con celular" />
                         <span className='descripcionSection'>Choose what to eat choosing from a variety of restaurants from the list</span>
                     </div>
-                    <button className='btn__next' >Next</button>
+                    <button className='btn__next' onClick={siguiente}>Next</button>
                 </section>
 
             </SwiperSlide>
@@ -41,7 +54,7 @@ const Slider = () => {
                         <img src={hombreCelular} alt="Mujer con celular" />
                         <span className='descripcionSection'>Choose where you want to deliver by indicating on the map</span>
                     </div>
-                    <button className='btn__next'>Next</button>
+                    <button className='btn__next' onClick={siguiente}>Next</button>
             </section>
             </SwiperSlide>
 
@@ -52,8 +65,12 @@ const Slider = () => {
                         <img src={mujerCelular2} alt="Mujer con celular" />
                         <span className='descripcionSection'>We will deliver as soon as possible</span>
                     </div>
-                    <button className='btn__next'>Next</button>
+                    <button className='btn__next' onClick={siguiente}>Next</button>
             </section>
+            </SwiperSlide>
+
+            <SwiperSlide className="hide"> 
+                <Login />
             </SwiperSlide>
 
         </Swiper>
